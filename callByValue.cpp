a00a3a1473732023ef8b3fcb692fa2a6d3fc867d
Ganta Vishnu <ganta.vishnu@BondEvalues-MacBook-Air.local>
2021-06-06 10:00:36 +0530

    - added a new file
    Signed-off-by: Ganta Vishnu <ganta.vishnu@BondEvalues-MacBook-Air.local>

---
+++ callByValue.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int increment(int &x){
+    cout<<" Address of x in increment :: " << &x << endl;
+    return ++x;
+}
+
+int incrementP(int* x){
+    cout<<" Address of x in incrementP :: " << x << endl;
+    return ++(*x);
+}
+
+int incrementV(int x){
+    cout<<" Address of x in incrementV :: " << &x << endl;
+    return ++x;
+}
+
+int main()
+{
+    int x = 5;
+    increment(x);
+    cout<<" x value :: "<< x << endl;
+    incrementP(&x);
+    cout<<" x value :: "<< x << endl;
+    incrementV(x);
+    cout<<" x value :: "<< x << endl;
+    return 0;
+}
