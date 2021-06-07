57103c6a2d64196cbffe61cd18da56eb254e8515
Ganta Vishnu <ganta.vishnu@BondEvalues-MacBook-Air.local>
2021-06-07 08:18:40 +0530

    - added fibinoci series using dynamic programming
    Signed-off-by: Ganta Vishnu <ganta.vishnu@BondEvalues-MacBook-Air.local>

---
+++ fibUsingDP.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int fibOld(int n){
+    if(n <= 2) return 1;
+    return fibOld(n-1) + fibOld(n-2);
+}
+
+int fib(int n, int* arr){
+    if(arr[n] > 0) return arr[n];
+    int n1 = fib(n-1, arr);
+    arr[n-1] = n1;
+    int n2 = fib(n-2, arr);
+    arr[n-2] = n2;
+    return n1 + n2;
+}
+
+int main(){
+    int n;
+    cout<<"Enter n value :: ";
+    cin>>n;
+    int* arr = new int[n+1]{0, 1, 1};
+    // current date/time based on current system
+    time_t now = time(0);
+    // convert now to string form
+    char* dt = ctime(&now);
+    cout << "The local date and time is Before :: " << dt << endl;
+    cout<<"fib("<<n<<") :: "<<fib(n, arr)<<endl;
+    now = time(0);
+    // convert now to string form
+    dt = ctime(&now);
+    cout << "The local date and time is After :: " << dt << endl;
+    
+    now = time(0);
+    // convert now to string form
+    dt = ctime(&now);
+    cout << "The local date and time is Before :: " << dt << endl;
+    cout<<"fibOld("<<n<<") :: "<<fibOld(n)<<endl;
+    now = time(0);
+    // convert now to string form
+    dt = ctime(&now);
+    cout << "The local date and time is After :: " << dt << endl;
+    
+    
+    return 0;
+}
