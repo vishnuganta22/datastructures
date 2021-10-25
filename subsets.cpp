f948e3896b6ee7e1a6808d8e1c908b9403a63147
Vishnu Ganta <vishnu.ganta22@gmail.com>
2021-10-25 17:01:38 -0400

    Update subsets.cpp
    Complete code for subsets

--- subsets.cpp
+++ subsets.cpp
@@ -2,6 +2,29 @@
 
 using namespace std;
 
+void subSetHelper(int* arr, int size, int start, int* subset, int subsetSize){
+    if(start == size) return;
+    if(subsetSize == size) return;
+    subSetHelper(arr, size, start + 1, subset, subsetSize);
+    int* temp = new int[subsetSize + 1];
+    cout<<"[ ";
+    for(int i = 0; i < subsetSize; i++){
+        temp[i] = subset[i];
+        cout<<temp[i]<<" , ";
+    }
+    temp[subsetSize] = arr[start];
+    cout<<temp[subsetSize]<<" ]"<<endl;
+    subset = temp;
+    subsetSize = subsetSize + 1;
+    start = start + 1;
+    subSetHelper(arr, size, start, subset, subsetSize);
+}
+
+void printAllSubsets(int* arr, int size){
+    cout<<"[ ]"<<endl;
+    subSetHelper(arr, size, 0, new int[0], 0);
+}
+
 int main()
 {
     int size;
@@ -10,5 +33,6 @@ int main()
     for(int i = 0; i < size; i++){
         cin>>arr[i];
     }
+    printAllSubsets(arr, size);
     return 0;
 }
