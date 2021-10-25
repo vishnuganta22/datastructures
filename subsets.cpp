4835997d683ca3411e172bd48f91b14689add6f9
Vishnu Ganta <vishnu.ganta22@gmail.com>
2021-10-25 09:30:57 -0400

    Update subsets.cpp
    cosmetic changes

--- subsets.cpp
+++ subsets.cpp
@@ -4,5 +4,11 @@ using namespace std;
 
 int main()
 {
+    int size;
+    cin>>size;
+    int *arr = new int[size];
+    for(int i = 0; i < size; i++){
+        cin>>arr[i];
+    }
     return 0;
 }
